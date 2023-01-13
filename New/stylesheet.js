1ceae11565c03e39f82f7e36331575e32ac6a929
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-01-13 21:34:40 +0100

    event listener and if statements

--- New/stylesheet.js
+++ New/stylesheet.js
@@ -4,3 +4,25 @@ const contacts = ["Abi: 2181088",
  "Joseph: 2183082",
  "Victoria: 2167457", "Younan: 2164537"];
 
+ const para = document.querySelector("P");
+ const input = document.querySelector(".input");
+ const btn = document.querySelector(".button");
+
+ btn.addEventListener("click", () => {
+    const searchName = input.ariaValueMax.toLowerCase();
+    input.value = " ";
+    input.focus();
+    para.textContent = " ";
+    for (const contact of contacts){
+        const splitContact = contact.split(":");
+        if (splitContact[0].toLowerCase() === searchName){
+            para.textContent =
+            `${splitContact[0]}'s number is ${splitContact[1]}.`
+                   break;
+                }          
+        }
+        if(para.textContent === " "){
+            para.textContent = "cContact not found.";
+        }
+ });
+
